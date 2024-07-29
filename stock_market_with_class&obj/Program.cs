d3d3f48911b501ff9684f27c9c0440b76b35fda9
Vishal Prajapati <vishalp951295@gmail.com>
2024-07-29 22:55:33 +0530

    Add project files.

---
+++ stock_market_with_class&obj/Program.cs
@@ -0,0 +1,6 @@
+﻿using stock_market_with_class_obj;
+
+stock stock = new stock();
+
+stock.input();
+stock.output();
---
+++ stock_market_with_class&obj/stock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stock_market_with_class_obj
+{
+    public class stock
+    {
+        public string stock_Name = "";
+        public string stock_symbol = "";
+        public double current_stock_price = 0;
+        public double previous_stock_price = 0;
+
+        public void input()
+        {
+            Console.WriteLine("enter stock name");
+            stock_Name = Console.ReadLine();
+            Console.WriteLine("enter stock symbole");
+            stock_symbol = Console.ReadLine();
+            Console.WriteLine("enter stock current price");
+            current_stock_price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter stock previous price");
+            previous_stock_price = Convert.ToDouble(Console.ReadLine());
+
+        }
+        public void output()
+        {
+            Console.WriteLine(stock_Name);
+            Console.WriteLine(stock_symbol);
+            if(current_stock_price > previous_stock_price)
+            {
+                Console.WriteLine("you are in profit of "+(current_stock_price-previous_stock_price));
+                Console.WriteLine("your profit in % is " + (((current_stock_price * 100) / previous_stock_price)-100)+"%");
+            }
+            else
+            {
+                Console.WriteLine("you are in loss of " + (previous_stock_price-current_stock_price));
+                Console.WriteLine("your loss in % is " + (100 - ((current_stock_price * 100) / previous_stock_price))+"%");
+            }
+        }
+    }
+}
